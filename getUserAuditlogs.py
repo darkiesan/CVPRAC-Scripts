65465c3fb3d373ca91b68094497627a8dbcc6f5e
Patrik Olsson <patrik.olsson@pfsw.com>
2018-05-30 18:16:28 +0200

    Anonymizing user/pass

--- getUserAuditlogs.py
+++ getUserAuditlogs.py
@@ -23,7 +23,7 @@ def get_logs(clnt):
 
 def main():
     clnt = CvpClient()
-    clnt.connect(['192.168.130.3'], 'cvpadmin', 'pzkpw51B', protocol='https')
+    clnt.connect(['192.168.130.3'], 'cvpadmin', 'mypass', protocol='https')
 
     logs = get_logs(clnt)
     print json.dumps(logs, indent=4, sort_keys=True)
